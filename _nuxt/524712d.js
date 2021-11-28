96e1463979a321dc1748ccdf0534157393f34c4d
tandpfun <tandpfun@users.noreply.github.com>
2021-11-28 04:35:28 +0000

    deploy: feee8926f338f54c2603be4ce0b46454fa8674e3

---
+++ _nuxt/524712d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{280:function(n,e,t){"use strict";t.r(e);var l=t(16),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("p")}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1638074127/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638074127",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
