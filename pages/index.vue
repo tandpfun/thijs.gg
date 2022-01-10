e38f89f3866138aaf61e9e392979bce3dc8b78c6
tandpfun <dev.tandpfun@gmail.com>
2022-01-09 18:52:47 -0800

    link to repo in footer

--- pages/index.vue
+++ pages/index.vue
@@ -19,8 +19,13 @@
       <Divider />
 
       <div class="copyright text-center mb-8">
-        &copy; {{ new Date().getFullYear() }} Coding#0001 | Built with NuxtJS &
-        TailwindCSS
+        &copy; {{ new Date().getFullYear() }} Coding#0001 |
+        <a
+          href="https://github.com/tandpfun/thijs.gg"
+          target="_blank"
+          class="hover:underline"
+          >Built with NuxtJS & TailwindCSS</a
+        >
       </div>
     </div>
   </div>
