034cd13e5681c8ce638483ed354c9f48e8a62176
tandpfun <tandpfun@users.noreply.github.com>
2024-04-23 18:37:39 +0000

    deploy: b7abc7678e6e64770c249adce56aed37c73a2286

--- _nuxt/static/1711333154/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1711333154",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
