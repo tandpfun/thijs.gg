f26b77b6a59c63e2251ec9519500e38710c1b2b7
Thijs <hi@thijs.gg>
2024-04-23 11:33:32 -0700

    change avatar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,9 +4,8 @@
       <div class="select-none">
         <h1 class="flex items-center">
           <img
-            src="~/assets/img/avatar.png"
+            src="~/assets/img/redpanda.png"
             class="w-10 h-10 rounded-lg inline avatar"
-            @click="toggleFurry"
           />
           <span class="ml-2 font-bold text-lg">thijs.gg</span>
         </h1>
@@ -17,23 +16,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    toggleFurry() {
-      const avatars = document.getElementsByClassName('avatar')
-      for (const a of avatars) {
-        a.src = a.src.includes('redpanda')
-          ? require('~/assets/img/avatar.png')
-          : '/redpanda.png'
-      }
-
-      const favicon = document.querySelectorAll('[rel="icon"]')[0]
-      favicon.href = favicon.href.includes('favicon')
-        ? '/redpanda.png'
-        : 'favicon.png'
-    },
-  },
-}
-</script>
